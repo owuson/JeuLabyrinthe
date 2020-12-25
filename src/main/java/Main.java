ac6f224a8354480cb14203cd59572da0a59aefb1
Phillip Owusu <owuson81@gmail.com>
2020-12-25 16:31:08 +0100

    3 eme commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,12 @@ public class Main {
 
     public static Scene scene;
 
-    // Creation de la fenêtre graphique de l'application
+
     public static void main(String[] args){
+        // Creation de la fenêtre graphique de l'application
         JFrame fenetre = new JFrame("Jeu du labyrinthe");
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fenetre.setSize(700, 360);
+        fenetre.setSize(1400, 720);
         fenetre.setLocationRelativeTo(null);
         fenetre.setResizable(false);
         fenetre.setAlwaysOnTop(true);
